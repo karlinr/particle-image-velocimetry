ea1b68685d07980caddc833574ec7e1b9f5479b4
Karlin Ross <karlinross@googlemail.com>
2021-10-15 22:32:08 +0100

    Initial commit

---
+++ create_animation.py
@@ -0,0 +1,116 @@
+import numpy as np
+import random
+import tifffile as tf
+import time
+
+import matplotlib.pyplot as plt
+
+# Animation variables
+animation_width = 352
+animation_height = 352
+animation_frames = 20
+particles = 10000
+particle_size = 5
+
+
+class Particle:
+    def __init__(self, _x, _y, _particle_type, _brightness):
+        # Assign particle variables
+        self.particle_type = _particle_type
+        self.brightness = _brightness
+
+        if self.particle_type == "circular":
+            self.radius = random.uniform(1, np.min([animation_width, animation_height]) / 2)
+            self.theta = random.uniform(0, 360)
+        else:
+            self.x = _x
+            self.y = _y
+            self.velocity_x = 0
+            self.velocity_y = 0
+            self.set_velocity()
+
+    def set_velocity(self):
+        # Particle motion
+        if self.particle_type == "potential":
+            self.velocity_x = 0
+            self.velocity_y = 1 + self.y / 80
+        elif self.particle_type == "constant":
+            self.velocity_x = 3
+            self.velocity_y = 3
+        elif self.particle_type == "random":
+            self.velocity_x = random.uniform(-3, 3)
+            self.velocity_y = random.uniform(-3, 3)
+        elif self.particle_type == "noise":
+            self.x = random.randint(0, animation_width - 1)
+            self.y = random.randint(0, animation_height - 1)
+
+    def step(self):
+        # Advance a frame
+        if self.particle_type == "circular":
+            # Update angle and position
+            self.theta += 2 / self.radius
+            self.x = animation_width / 2 + self.radius * np.cos(self.theta)
+            self.y = animation_height / 2 + self.radius * np.sin(self.theta)
+        else:
+            # Update velocity and position
+            self.set_velocity()
+            self.x += self.velocity_x
+            self.y += self.velocity_y
+
+            # Handle out of frame
+            if self.x >= animation_width:
+                self.x -= animation_width
+            if self.x < 0:
+                self.x += animation_width
+            if self.y >= animation_height:
+                self.y -= animation_height
+            if self.y < 0:
+                self.y += animation_height
+
+    def get_position(self):
+        # Returns particle position as a tuple
+        return int(self.x), int(self.y)
+
+    def get_brightness(self):
+        # Returns particle brightness
+        return self.brightness
+
+
+def make_animation(_particle_type):
+    start = time.time()
+    print(f"Creating animation of type {_particle_type}...", end = "")
+
+    # Setup particles
+    a = np.empty(particles + 1, dtype=object)
+    for i in range(particles):
+        _x = random.randint(0, animation_width - 1)
+        _y = random.randint(0, animation_height - 1)
+        a[i] = Particle(_x, _y, _particle_type, random.randint(0, 255))
+
+    # Set up video array
+    video_array = np.zeros((animation_frames, animation_width, animation_height), dtype=np.int8)
+
+    # Simulate particles
+    for t in range(animation_frames):
+        image_array = np.zeros((animation_width, animation_height), dtype=np.int8)
+        for i in range(particles):
+            a[i].step()
+            image_array[a[i].get_position()] = a[i].get_brightness()
+        # Save current frame to video
+        video_array[t] = image_array
+
+    # Save to disk
+    # noinspection PyTypeChecker
+    tf.imwrite(f"data/animations/animation_{_particle_type}.tif", video_array, compression = "zlib")
+
+    # Measure execution time
+    end = time.time()
+    print(f" completed in {end - start:.2f} seconds")
+
+
+# Make videos
+make_animation("circular")
+make_animation("random")
+make_animation("constant")
+make_animation("potential")
+make_animation("noise")
---
+++ get_velocity_field_ref.py
@@ -0,0 +1,151 @@
+import numpy as np
+import tifffile as tf
+import matplotlib.pyplot as plt
+import matplotlib.animation as animate
+import os
+import time
+# from scipy.signal import correlate2d
+
+# Settings
+# plt.style.use('dark_background')
+
+
+def get_particle_velocity_from_video(_filename, _iw1, _iw2, _frame_spacing):
+    start = time.time()
+    print(f"Calculating velocity field for {_filename}... ")
+
+    # Get vars
+    iw1 = _iw1
+    iw2 = _iw2
+    frame_spacing = _frame_spacing
+
+    # Import video and get attributes
+    video = tf.imread(_filename).astype(np.int8)
+    frames = video.shape[0] - frame_spacing
+    width = int(np.floor((video.shape[1]) / iw2))
+    height = int(np.floor((video.shape[2]) / iw2))
+
+    # Initialise arrays
+    velocity_field = np.empty((frames, width, height, 4))
+    mean_velocity_field = np.empty((width, height, 4))
+    absolute_differences = np.empty((frames, width, height, iw2 - iw1, iw2 - iw1))
+
+    for i in range(0, frames):
+        print(f"{i}/{frames}")
+        b = video[i + frame_spacing]
+        a = video[i]
+
+        #fig, ax = plt.subplots(figsize = (20, 20), sharex = True)
+
+        # Find the velocity field using the sum of absolute differences
+        for j in range(0, width):
+            for k in range(0, height):
+
+                # Initialise arrays
+                abs_diff_map = np.zeros((iw2 - iw1, iw2 - iw1))
+
+                # Get center of iw2
+                x = int((j + 0.5) * iw2)
+                y = int((k + 0.5) * iw2)
+
+                # Get slice of image at larger interrogation window
+                iw2_a = a[int(x - iw2 / 2):int(x + iw2 / 2), int(y - iw2 / 2):int(y + iw2 / 2)]
+                iw2_b = b[int(x - iw2 / 2):int(x + iw2 / 2), int(y - iw2 / 2):int(y + iw2 / 2)]
+
+                # Get slices for smaller interrogation window
+                tl = int((iw2 - iw1) / 2)
+                br = int((iw2 + iw1) / 2)
+                iw1_a = iw2_a[tl:br, tl:br]
+                iw1_b = iw2_b[tl:br, tl:br]
+
+                # Check if there is no motion detected before attempting to find flow vectors
+                """if np.array_equiv(iw1_a, iw1_b):
+                    abs_diff_map[int((iw2 - iw1) / 2), int((iw2 - iw1) / 2)] = float("-inf")
+                    velocity_field[i, j, k, :] = [x, y, 0, 0]
+                else:"""
+                # Calculate the absolute differences for the interrogation window
+                for m in range(0, iw2 - iw1):
+                    for n in range(0, iw2 - iw1):
+                        iw1_a = iw2_a[m:m + iw1, n:n + iw1]
+                        abs_diff_map[m, n] = np.sum(np.abs(iw1_a - iw1_b))
+
+                #fig.add_subplot(width, height, k * height + j + 1)
+                #plt.imshow(abs_diff_map)
+                #plt.axis('off')
+
+                # Get the minima of the absolute differences to find the velocity vector
+                peak_position = np.unravel_index(abs_diff_map.argmin(), abs_diff_map.shape)
+                u = peak_position[0] - ((iw2 - iw1) / 2)
+                v = peak_position[1] - ((iw2 - iw1) / 2)
+
+                # Save to the arrays
+                velocity_field[i, j, k, :] = [x, y, u, v]
+                absolute_differences[i, j, k, :, :] = abs_diff_map
+
+        #plt.show()
+
+    # Calculate the mean velocity field using the time averaged array of absolute differences
+    mean_absolute_differences = np.mean(absolute_differences, axis = 0)
+    for j in range(0, width):
+        for k in range(0, height):
+            x = int((j + 0.5) * iw2)
+            y = int((k + 0.5) * iw2)
+
+            # Get the minima of the absolute differences to find the average velocity vector
+            peak_position = np.unravel_index(mean_absolute_differences[j, k].argmin(),
+                                             mean_absolute_differences[j, k].shape)
+            u_avg = peak_position[0] - ((iw2 - iw1) / 2)
+            v_avg = peak_position[1] - ((iw2 - iw1) / 2)
+
+            # Save to the arrays
+            mean_velocity_field[j, k, :] = [x, y, u_avg, v_avg]
+
+    end = time.time()
+    print(f"Completed in {(end - start):.2f} seconds")
+    return velocity_field, mean_velocity_field, video.shape[1], video.shape[2], absolute_differences
+
+
+def plot_fields(_animation):
+    field = get_particle_velocity_from_video(f"./data/animations/{_animation}", 24, 56, 1)
+
+    x = field[1][:, :, 0]
+    y = field[1][:, :, 1]
+    u = field[1][:, :, 2]
+    v = field[1][:, :, 3]
+    plt.title(f"Time averaged velocity field for {_animation}")
+    mag = np.sqrt(pow(np.array(field[1][:, :, 2]), 2) + pow(np.array(field[1][:, :, 3]), 2))
+    plt.quiver(x, y, u, v, mag, cmap = "viridis")
+    plt.colorbar()
+    plt.xlim(0, field[2])
+    plt.ylim(0, field[3])
+    plt.show()
+
+    x = field[0][0, :, :, 0]
+    y = field[0][0, :, :, 1]
+    u = field[0][0, :, :, 2]
+    v = field[0][0, :, :, 3]
+    plt.title(f"Velocity field for {_animation}")
+    mag = np.sqrt(pow(np.array(field[0][0, :, :, 2]), 2) + pow(np.array(field[0][0, :, :, 3]), 2))
+    plt.quiver(x, y, u, v, mag, cmap = "viridis")
+    plt.colorbar()
+    plt.xlim(0, field[2])
+    plt.ylim(0, field[3])
+    plt.show()
+
+    fig, ax = plt.subplots(1, 1)
+    q = ax.quiver(x, y, u, v, cmap = "viridis")
+
+    def update_quiver(_f, _q):
+        _q.set_UVC(field[0][_f, :, :, 2], field[0][_f, :, :, 3])
+        return _q,
+
+    anim = animate.FuncAnimation(fig, update_quiver, fargs = (q,), frames = field[0].shape[0], interval = 120,
+                                 blit = False)
+
+    writervideo = animate.FFMpegWriter(fps = 5)
+    anim.save(f"./data/animated_fields/anim_{_animation}.mp4", writer = writervideo)
+    plt.close(fig)
+
+
+for animation in os.listdir("./data/animations/"):
+    plot_fields(animation)
